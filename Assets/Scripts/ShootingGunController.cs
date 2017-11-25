ef4c0d2220151975a17d385c5cdb604c1b7e6199
4419-2-7\user <kk159357222@gmail.com>
2017-11-25 10:03:28 +0800

    2.視角移動

--- Assets/Scripts/ShootingGunController.cs
+++ Assets/Scripts/ShootingGunController.cs
@@ -12,7 +12,14 @@ public class ShootingGunController : MonoBehaviour
     public Transform gunEnd;
     public LineRenderer gunFlare;
     public float gunFlareVisibleSeconds = 0.07f;
-
+    //......................................
+    public Transform cameraTransform;
+    public Reticle reticle;
+    public Transform gunContainer;
+    public float damping = 0.5f;
+    public float dampingCoef = 20f;
+    public float gunContainerSmooth = 10f;
+    //......................................
     private void OnEnable()
     {
         vrInput.OnDown += HandleDown;//事件用法+=
@@ -39,7 +46,7 @@ public class ShootingGunController : MonoBehaviour
         yield return StartCoroutine(MoveLineRenderer(lineLength)); //先回去上面，在往下
         gunFlare.enabled = false;
     }
-       private IEnumerator MoveLineRenderer(float lineLength)
+    private IEnumerator MoveLineRenderer(float lineLength)
             {
             float timer = 0f;
             while (timer < gunFlareVisibleSeconds)
@@ -50,6 +57,26 @@ public class ShootingGunController : MonoBehaviour
                 timer += Time.deltaTime;
             }
         }
+
+    private void Update()
+    {
+        transform.rotation=Quaternion.Slerp(transform.rotation,InputTracking.GetLocalRotation(VRNode.Head),damping * (1 - Mathf.Exp(dampingCoef * Time.deltaTime)));
+        transform.position = cameraTransform.position;
+        Quaternion lookAtRotation = Quaternion.LookRotation(reticle.ReticleTransform.position - gunContainer.position);
+        gunContainer.rotation = Quaternion.Slerp(gunContainer.rotation, lookAtRotation, gunContainerSmooth * Time.deltaTime);
     }
+
+
+
+}
+
+
+
+
+
+
+
+
+
 
 
