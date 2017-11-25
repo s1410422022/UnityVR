397595ca4ce5b5b6393539929498fee1eac266c5
4419-2-7\user <kk159357222@gmail.com>
2017-11-25 09:42:13 +0800

    1.槍做

--- Assets/Scripts/ShootingGunController.cs
+++ Assets/Scripts/ShootingGunController.cs
@@ -5,5 +5,51 @@ using VRStandardAssets.Utils;
 using UnityEngine.VR;
 public class ShootingGunController : MonoBehaviour
 {
+    public AudioSource audiosource;
+    public VRInput vrInput;
+    public float defaultLineLength = 70f; //雷射長度
+    public ParticleSystem flareParticle;
+    public Transform gunEnd;
+    public LineRenderer gunFlare;
+    public float gunFlareVisibleSeconds = 0.07f;
 
-}
+    private void OnEnable()
+    {
+        vrInput.OnDown += HandleDown;//事件用法+=
+
+    }
+    private void OnDisable()
+    {
+        vrInput.OnDown -= HandleDown;//有+=就要有-=
+    }
+
+    private void HandleDown()       //方法
+    {
+       
+        StartCoroutine(Fire());
+       
+    }
+    private IEnumerator Fire()
+    {
+        audiosource.Play(); 
+    
+        float lineLength = defaultLineLength;   //todo判斷有無射到東西
+        flareParticle.Play();                   //粒子
+        gunFlare.enabled = true;
+        yield return StartCoroutine(MoveLineRenderer(lineLength)); //先回去上面，在往下
+        gunFlare.enabled = false;
+    }
+       private IEnumerator MoveLineRenderer(float lineLength)
+            {
+            float timer = 0f;
+            while (timer < gunFlareVisibleSeconds)
+            {
+                gunFlare.SetPosition(0, gunEnd.position);                              //起點
+                gunFlare.SetPosition(1, gunEnd.position + gunEnd.forward*lineLength);  //正方向
+                yield return null;
+                timer += Time.deltaTime;
+            }
+        }
+    }
+
+
