a259eb6dc5aae8aff07a8f6eaa34f01fffdbd617
4419-2-7\user <kk159357222@gmail.com>
2017-11-25 14:20:35 +0800

    4.打碎靶子

--- Assets/Scripts/ShootingTarget.cs
+++ Assets/Scripts/ShootingTarget.cs
@@ -7,5 +7,63 @@ using VRStandardAssets.Common;
 
 public class ShootingTarget : MonoBehaviour
 {
+    public int score = 1;
+    public float destroyTimeOutDuration=2f;
+    public event Action<ShootingTarget>OnRemove;
+    private Transform cameraTransform;
+    private AudioSource audioSource;
+    private VRInteractiveItem vrInteractiveItem;
+    private Renderer mRenderer;
+    private Collider mCollider;
+    public AudioClip destroyClip;
+    public GameObject destroyPrefab;
+    private bool isEnding;
 
+    private void Awake()
+    {
+        cameraTransform = Camera.main.transform;
+        audioSource = GetComponent<AudioSource>();
+        vrInteractiveItem = GetComponent<VRInteractiveItem>();
+        mRenderer = GetComponent<Renderer>();
+        mCollider = GetComponent<Collider>();
+
+    }
+
+    private void OnEnable()
+    {
+        vrInteractiveItem.OnDown += HandDown;
+    }
+
+    private void OnDisable()
+    {
+        vrInteractiveItem.OnDown -= HandDown;
+    }
+
+
+    private void OnDestroy()
+    {
+        OnRemove = null;
+    }
+
+
+    private void HandDown()
+    {
+        StartCoroutine(OnHit());
+
+    }
+
+    private IEnumerator OnHit()
+    {
+        mRenderer.enabled = false;
+        mCollider.enabled = false;
+        audioSource.clip = destroyClip;
+        audioSource.Play();
+        SessionData.AddScore(score);
+        GameObject destroyedTarget = Instantiate<GameObject>(destroyPrefab, transform.position, transform.rotation);
+        Destroy(destroyedTarget,destroyTimeOutDuration);
+        yield return new WaitForSeconds(destroyClip.length);
+        if (OnRemove != null)
+            OnRemove(this);
+
+    }
 }
